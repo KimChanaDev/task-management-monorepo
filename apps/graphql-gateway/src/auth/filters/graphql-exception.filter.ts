512ff066e5a7871d0c9d4d62336a5da430d5fa29
KimChanaDev <chanaweestr@gmail.com>
2025-12-03 09:19:22 +0000

    fix(graphql-gate): fix exception filter error

--- apps/graphql-gateway/src/auth/filters/graphql-exception.filter.ts
+++ apps/graphql-gateway/src/auth/filters/graphql-exception.filter.ts
@@ -1,33 +1,53 @@
-import { Catch, HttpException } from '@nestjs/common';
+import { Catch, HttpException, HttpStatus } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql';
 @Catch()
 export class GraphQLExceptionFilter implements GqlExceptionFilter {
-  catch(exception: HttpException) {
+  catch(exception: unknown) {
     if (exception instanceof GraphQLError) {
       return exception;
     }
-    const response: string | object = exception.getResponse();
-    const status = exception.getStatus();
-    let message = exception.message;
-    let validationErrors: any[] = [];
-    let error: string | undefined = undefined;
-    // if response is object and has message array (from ValidationPipe)
-    if (typeof response === 'object' && response['message']) {
-      error = response['error'];
-      const responseMessage = response['message'];
-      if (Array.isArray(responseMessage)) {
-        message = responseMessage.join(', ');
-        validationErrors = responseMessage;
-      } else {
-        message = responseMessage;
+
+    if (exception instanceof HttpException) {
+      const response: string | object = (exception as HttpException).getResponse();
+      const status = (exception as HttpException).getStatus();
+      let message = (exception as HttpException).message;
+      let validationErrors: any[] = [];
+      let error: string | undefined = undefined;
+
+      // if response is object and has message array (from ValidationPipe)
+      if (typeof response === 'object' && response['message']) {
+        error = response['error'];
+        const responseMessage = response['message'];
+        if (Array.isArray(responseMessage)) {
+          message = responseMessage.join(', ');
+          validationErrors = responseMessage;
+        } else {
+          message = responseMessage;
+        }
       }
+
+      return new GraphQLError(message, {
+        extensions: {
+          error,
+          statusCode: status,
+          validationErrors,
+        },
+      });
     }
-    return new GraphQLError(message, {
+
+    if (exception instanceof Error) {
+      return new GraphQLError(exception.message, {
+        extensions: {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        },
+      });
+    }
+
+    // else
+    return new GraphQLError('Internal server error', {
       extensions: {
-        error,
-        statusCode: status,
-        validationErrors,
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
       },
     });
   }
